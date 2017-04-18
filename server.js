711974de3b3e3fc0195ae4ab910b5ddf96bac165
bobbyschank <robertschank@gmail.com>
2017-04-18 13:54:22 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// import requirements
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// instantiate database
+let db = require('./models');
+
+// Express app called app
+let app = express();
