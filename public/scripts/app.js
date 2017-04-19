f781298b20cf39d37cadfd57bbc6418e4deafd8b
bobbyschank <robertschank@gmail.com>
2017-04-19 12:16:38 -0600

    get selected word

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+	// insertnNoteButton click
 	$('#insertNoteButton').click(function() {
 		console.log('insertNoteButton CLICKED!');
+		// ajax call to get notes (INDEX)
 		$.ajax({
 			method: 'GET',
 			url: '/api/notes',
@@ -11,15 +12,46 @@ $(document).ready(function(){
 			error: handleError
 		});
 	});
-
+	// testButton click
 	$('#testButton').click(function() {
 		console.log('insertNoteButton CLICKED!');
 		let pos =$('#textarea').getCursorPosition();
 		console.log('pos: ' + pos);
+		let textareaString = $('#textarea').val();
+		console.log('initial click char at pos: ' + textareaString.charAt(pos));
+		findWordAtPos(pos, textareaString);
 	});
-
 });
+// ^^ End of document.ready ^^
 
+function findWordAtPos(pos, textareaString) {
+	// set i to the cursor position
+	let i = pos;
+	// instantiate selectedWord
+	let selectedWord = '';
+
+	// keep moving "backwards" along the textString
+	// until we reach whitespace, aka the start of the word.
+	if (textareaString.charAt(i) === ' ') {i--;}
+	while (textareaString.charAt(i) !== ' ' && i > -1) {
+		i--;
+	}
+	console.log('while broken. i = ' + i);
+	console.log('character at i: ' + textareaString.charAt(i));
+
+	// move forward in textString, add each character to selectedWord
+	// until we get to the end of the word.
+	i++;
+	while (textareaString.charAt(i) !== ' ' && i < textareaString.length) {
+		//add each character to selectedWord
+		selectedWord += textareaString.charAt(i);
+		i++;
+	}
+	console.log('selectedWord: ' + selectedWord);
+	return selectedWord;
+}
+
+// When index comes back:
 function insertSuccess(json) {
 	notes = json;
 	let textareaString = $('#textarea').val();
@@ -30,6 +62,7 @@ function handleError(e) {
 	console.log('there was an error: e');
 }
 
+// This function determines the cursor's position in a string:
 (function ($, undefined) {
     $.fn.getCursorPosition = function() {
         var el = $(this).get(0);
